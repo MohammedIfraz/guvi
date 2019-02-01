3a14a8d59819b4639b81d416dd7150dbc82fc370
Mohammed Ifraz C <mohammedifraz.ee@gmail.com>
2019-01-31 21:42:32 -0800

    Update num.py

--- num.py
+++ num.py
@@ -1,7 +1,5 @@
-checktype = input ('enter a number')
-if checktype>0:
-    print('positive')
-elif checktype<0:
-    print('negative')
-else:
-    print('zero')
+check=int(input())
+if(check>0):
+    print('Positive')
+else(check<0):
+    print('Negative')
